87dc768bc52365630094b2ecdc159189568227ad
KRadic <kristian.radic@gmail.com>
2021-08-26 11:52:25 +0200

    -add: settings menu, no audio yet

---
+++ Minigame/Assets/Scripts/GameControllers/SettingsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsController : USceneController
+{
+    public SettingsController() : base(Names.Settings) { }
+
+    public override void SceneDidLoad()
+    {
+        AddListeners();
+    }
+
+    public override void SceneWillAppear()
+    {
+        var obj = GameObject.Find(Names.Settings);
+    }
+
+    public override void SceneWillDisappear()
+    {
+        base.SceneWillDisappear();
+    }
+
+    private void AddListeners()
+    {
+        var BtnMainMenu = GameObject.Find(Names.BtnMainMenu).GetComponent<Button>();
+        if (BtnMainMenu == null) return;
+
+        BtnMainMenu.onClick.AddListener(() =>
+        {
+            var mainMenu = new MainMenuController();
+            UNavigationController.SetRootViewController(mainMenu);
+        });
+    }
+}
--- Minigame/Assets/Scripts/Utils/Names.cs
+++ Minigame/Assets/Scripts/Utils/Names.cs
@@ -2,11 +2,19 @@
 {
     //Scenes
     public static string MainMenu = "MainMenuScene";
+    public static string Settings = "SettingsScene";
 
     //MainMenu
     public static string BtnPlay = "BtnPlay";
     public static string BtnSettings = "BtnSettings";
     public static string BtnExit = "BtnExit";
+
+    //Settings
+    public static string BtnSoundOff = "BtnSoundOff";
+    public static string BtnSoundOn = "BtnSoundOn";
+    public static string VfxSlider = "VfxSlider";
+    public static string MusicSlider = "MusicSlider";
+    public static string BtnMainMenu = "BtnMainMenu";
 
     //Managers
     public static string InputManager = "InputManager";
