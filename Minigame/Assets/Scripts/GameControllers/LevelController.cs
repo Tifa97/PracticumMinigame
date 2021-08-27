ba46ec8f1ea20fa3e29cb5769e21690583a91826
KRadic <kristian.radic@gmail.com>
2021-08-27 15:30:40 +0200

    -add: level with a player that has a working gun

---
+++ Minigame/Assets/Scripts/GameControllers/LevelController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : USceneController
+{
+    public LevelController() : base(Names.Level) { }
+
+    public override void SceneDidLoad()
+    {
+        
+    }
+
+    public override void SceneWillAppear()
+    {
+        var obj = GameObject.Find(Names.Level);
+    }
+
+    public override void SceneWillDisappear()
+    {
+        base.SceneWillDisappear();
+    }
+}
---
+++ Minigame/Assets/Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameAsset bullet = GameAsset.Bullet;
+    public GameObject weapon;
+    public float bulletVelocity = 10f;
+    public float fireRate = 0.2f;
+    public Transform bulletPosition;
+
+    void Start()
+    {
+        
+    }
+
+    public void Fire()
+    {
+        var shot = AssetProvider.GetAsset(bullet);
+        shot.GetComponent<BulletController>().PrepareBullet(bulletPosition, bulletVelocity, Names.Enemy, 1f);
+        shot.transform.position = bulletPosition.position;
+        shot.transform.rotation = bulletPosition.rotation;
+    }
+
+    void Update()
+    {
+        
+    }
+}
