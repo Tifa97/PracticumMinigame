e223473d7d8d4d6f02e67a438290af57c3fff422
KRadic <kristian.radic@gmail.com>
2021-08-28 21:46:36 +0200

    -add: enemies with bad AI

---
+++ Minigame/Assets/Scripts/Enemy/EnemyAi.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform targetPosition;
+    public GameAsset rangedAsset = GameAsset.Bullet;
+    public PlayerController player;
+    //player health
+    public float damage = 1f;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        //player health
+        targetPosition = FindObjectOfType<PlayerController>().gameObject.transform;
+        agent.SetDestination(targetPosition.position);
+    }
+
+    void Update()
+    {
+        if (targetPosition == null)
+        {
+            targetPosition = FindObjectOfType<PlayerController>().gameObject.transform;
+        }
+    }
+}
